551f184a0ea1d84b14ee4467879375ba9308ea05
sebawars <sebastianmoralesnap@gmail.com>
2021-04-09 22:31:56 -0300

    Se cambia icono por pochoclos

--- front/src/components/NavBar/index.js
+++ front/src/components/NavBar/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link, Nav, Title } from './styles';
+import { Link, Nav, Title, PopCornIcon } from './styles';
 import { FcFilmReel } from 'react-icons/fc';
 import { FiPower } from 'react-icons/fi';
 import { useSelector, useDispatch } from 'react-redux';
 import { setAuth as setAuthAction } from '../../redux/actions/set-auth';
 import { removeTokenStorage } from '../../util/storage';
+import logo from '../../assets/popcorn.svg';
 
 const NavBar = () => {
   // Redux
@@ -20,9 +21,10 @@ const NavBar = () => {
   return (
     <Nav>
       <Link to='/'>
-        <div>
+        {/* <IconContainer>
           <FcFilmReel size='35px' />
-        </div>
+        </IconContainer> */}
+        <PopCornIcon src={logo} alt='Logo' />
         <Title>VideoClub</Title>
       </Link>
       {auth && (
--- front/src/components/NavBar/styles.js
+++ front/src/components/NavBar/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Link as LinkRouter } from '@reach/router';
 
 export const Nav = styled.nav`
@@ -36,4 +36,23 @@ export const Title = styled.h1`
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
     'Helvetica Neue', sans-serif;
   color: #90a4ae;
+  &:hover {
+    transform: scale(1.08, 1.08);
+  }
+  transition: all 0.3s ease-in-out;
+`;
+
+const scale = keyframes`
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.3);
+  }
+`;
+
+export const PopCornIcon = styled.img`
+  animation: ${scale} 1s linear infinite;
+  animation-direction: alternate;
+  max-height: 45px;
 `;
